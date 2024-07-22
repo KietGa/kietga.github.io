b199f3df28200cdf73f05df7f02dba67c3317909
KigaStudio <studiokiga@gmail.com>
2024-07-22 22:48:33 +0700

    Update tiep 17

---
+++ web/auto_text/index.js
@@ -0,0 +1,27 @@
+const containerEl = document.querySelector(".container");
+const careers = ["Youtuber", "Web Developer", "Freelancer", "Student"]
+
+let careersIndex = 0;
+let characterIndex = 0;
+
+updateText();
+
+function updateText()
+{
+    characterIndex++;
+    containerEl.innerHTML = `<h1>I am a ${careers[careersIndex].slice(0, characterIndex)}</h1>`;
+    
+    if (characterIndex === careers[careersIndex].length)
+    {
+        careersIndex++;
+        characterIndex = 0;
+    }
+    if (careersIndex === careers.length)
+    {
+        careersIndex = 0;
+    }
+
+    setTimeout(updateText, 400);
+}
+
+
